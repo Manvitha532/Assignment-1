8a3aedfbc8e2a96c550bc69562e86ace3ce19f23
Manvitha532 <manvithachinthala180@gmail.com>
2026-01-21 16:08:50 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+if num2 != 0:
+    division = num1 / num2
+else:
+    division = "Cannot divide by zero"
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
